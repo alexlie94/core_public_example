419195b7d9ecd50236f22db09c73a55c812294d7
alex <alexanderlieepm@gmail.com>
2024-03-12 18:44:40 +0700

    update

--- customers/inventory_group/views/v_select_image_default.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?>
-
-<input type="hidden" id="group_default_id" name="group_default_id" value="<?= $default_id ?>">
-
-<div class="row text-center">
-    <h1><?= $image_name ?></h1>
-</div>
